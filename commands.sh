805c4938ffb5214ef632202ee7fa033b2ea13bde
gukim@postech.ac.kr <koimg8545@naver.com>
2026-07-14 21:09:58 +0900

    first commit

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python -u preprocess.py --data_path data/truck 
-CUDA_VISIBLE_DEVICES=0 python train_features.py -s data/truck -m output/truck
-CUDA_VISIBLE_DEVICES=0 python render_features.py -m output/truck --save_ply --style_dir styles
+CUDA_VISIBLE_DEVICES=0 python -u preprocess.py --data_path data/garden 
+CUDA_VISIBLE_DEVICES=0 python train_features.py -s data/garden -m output/garden
+CUDA_VISIBLE_DEVICES=0 python render_features.py -m output/garden --save_ply --style_dir styles
