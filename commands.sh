1a802d3bac30f8e54c4256275d99c4809865bfb6
GeonU Kim <32728514+Kim-GeonU@users.noreply.github.com>
2026-07-14 21:04:11 +0900

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,3 @@
-# CUDA_VISIBLE_DEVICES=0 python -u preprocess.py --data_path data/truck 
-# CUDA_VISIBLE_DEVICES=0 python train_features.py -s data/truck -m output/truck
-# CUDA_VISIBLE_DEVICES=0 python render_features.py -m output/truck --save_ply --style_dir styles
-CUDA_VISIBLE_DEVICES=0 python preprocess.py --data_path data/truck 
+CUDA_VISIBLE_DEVICES=0 python -u preprocess.py --data_path data/truck 
 CUDA_VISIBLE_DEVICES=0 python train_features.py -s data/truck -m output/truck
-CUDA_VISIBLE_DEVICES=0 python render_features.py -m output/truck --save_ply --style_dir styles_table
+CUDA_VISIBLE_DEVICES=0 python render_features.py -m output/truck --save_ply --style_dir styles
